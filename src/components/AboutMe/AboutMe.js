bdb65cb0af60c45318c2642f5c1eec522a5e56eb
Nikita Titov <netitov@gmail.com>
2021-03-27 11:56:09 +0300

    create comp. aboutme

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,35 @@
+import picPath from '../../images/testpic.png';
+
+
+function AboutMe() {
+
+  return (
+    <section className="aboutMe">
+      <h2 className="aboutMe__headng aboutProject__heading">Студент</h2>
+      <div className="aboutMe__section">
+        <div className="aboutMe__info">
+          <h3 className="aboutMe__name promo__heading">Никита</h3>
+          <p className="aboutMe__occupation">Фронтенд-разработчик, 27 лет</p>
+          <p className="aboutMe__disciption">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
+          и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал
+          в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами
+          и ушёл с постоянной работы.</p>
+          <ul className="aboutMe__contacts">
+            <a className="aboutMe__link" href="">
+              <li className="">Facebook</li>
+            </a>
+            <a className="aboutMe__link" href="">
+              <li className="">Github</li>
+            </a>
+          </ul>
+        </div>
+
+        <img className="aboutMe__pic" src={picPath} alt="Фото студента"/>
+      </div>
+
+    </section>
+
+  )
+}
+
+export default AboutMe;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,6 +6,7 @@ import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
 
 function Main() {
 
@@ -15,6 +16,7 @@ function Main() {
       <Promo />
       <AboutProject />
       <Techs />
+      <AboutMe />
     </main>
   )
 }
