1818bafa30392d76342703bcba20f21239ec3f7c
Nikita Titov <netitov@gmail.com>
2021-04-05 22:15:06 +0300

    create comp preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,10 +1,14 @@
-function Preloader() {
+import React from 'react'
+import './Preloader.css'
 
-  return (
-    <div className="preloader">
+const Preloader = () => {
+    return (
+        <div className="preloader">
+            <div className="preloader__container">
+                <span className="preloader__round"></span>
+            </div>
+        </div>
+    )
+};
 
-    </div>
-  )
-}
-
-export default Preloader;
+export default Preloader
